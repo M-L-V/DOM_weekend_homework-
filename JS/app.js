de76073445fab99cbce63a8437112eb1c37cb8ca
Morgaine <morgainedv@gmail.com>
2019-11-22 19:14:11 +0000

    mvp done

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const newItemform = document.querySelector('#new-item-form');
   newItemform.addEventListener('submit', handleNewItemFormSubmit);
 
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAll);
 })
 
 const handleNewItemFormSubmit = function (event) {
@@ -12,4 +14,32 @@ const handleNewItemFormSubmit = function (event) {
   foodList.appendChild(foodListItem);
 
   event.target.reset();
+}
+
+const createFoodListItem = function (form) {
+  const foodListItem = document.createElement('li');
+  foodListItem.classList.add('food-list-item');
+
+  const name = document.createElement('h2');
+  name.textContent = form.name.value;
+  foodListItem.appendChild(name);
+
+
+  const country = document.createElement('h3');
+  country.textContent = form.country.value;
+  foodListItem.appendChild(country);
+
+
+  const type = document.createElement('h3');
+  type.textContent = form.type.value;
+  foodListItem.appendChild(type);
+
+
+  return foodListItem;
+}
+
+
+const handleDeleteAll = function (event) {
+  const foodList = document.querySelector('#favourite-foods');
+  foodList.innerHTML = 'No foods left :(';
 }
